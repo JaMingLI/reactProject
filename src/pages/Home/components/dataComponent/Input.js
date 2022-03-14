14fbb7eb0f5a7531e13fc1036d76db735a034c56
li jaming <kampfer06@gmail.com>
2022-03-15 01:18:00 +0800

    add ok 北投區

--- src/pages/Home/components/dataComponent/Input.js
+++ src/pages/Home/components/dataComponent/Input.js
@@ -10,18 +10,18 @@ const InputSelect = ({select}) =>{
         <div className="inputArea">
             <h1>地區</h1>            
             <select className="area" onChange={selectdata}>
-                <option value='1' >松山區</option>  
-                <option value='2' >信義區</option> 
-                <option value='3' >大安區</option> 
-                <option value='4' >中山區</option> 
-                <option value='5' >中正區</option> 
-                <option value='6' >大同區</option> 
-                <option value='7' >萬華區</option> 
-                <option value='8' >文山區</option> 
-                <option value='9' >南港區</option> 
-                <option value='10'>內湖區</option> 
-                <option value='11'>士林區</option> 
-                <option value='12'>北投區</option> 
+                <option value='0' >松山區</option>  
+                <option value='1' >信義區</option> 
+                <option value='2' >大安區</option> 
+                <option value='3' >中山區</option> 
+                <option value='4' >中正區</option> 
+                <option value='5' >大同區</option> 
+                <option value='6' >萬華區</option> 
+                <option value='7' >文山區</option> 
+                <option value='8' >南港區</option> 
+                <option value='9'>內湖區</option> 
+                <option value='10'>士林區</option> 
+                <option value='11'>北投區</option> 
             </select>
         </div>
     )
